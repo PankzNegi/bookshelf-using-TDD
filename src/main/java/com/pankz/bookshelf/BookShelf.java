89532eaae053346b5c7fde69e6c707416d64c86d
unknown <pankznegi4638@gmail.com>
2023-02-26 19:44:19 +0530

    creating bookshelf using OOA&D

---
+++ src/main/java/com/pankz/bookshelf/BookShelf.java
@@ -0,0 +1,34 @@
+package com.pankz.bookshelf;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BookShelf {
+    private Set<Book> books; //Store our books
+
+    public BookShelf() {
+        this.books = new HashSet<>();
+    }
+
+    public void add(Book book) {
+        boolean added = this.books.add(book); //Adding books to our collection
+        //Below condition is according to add method implementation of sets hashcode & equal
+        if(!added)   //if book is not added because of equality operation of hashcode(here added=false thus !added become true
+        {
+            throw new IllegalArgumentException("You can't add same book twice!!!");
+        }
+
+
+    }
+
+    public int numberOfBooks() {
+        return  this.books.size() ; //Returning the size of book in bookshelf
+    }
+
+    public boolean delete(Book book) {
+
+        return this.books.remove(book);//Here also as per the contract of remove method in "set.java"(remove())
+        //As "Book" implements equals and hashcode method so if the book is present, it willfirst find the bucket  using the hashcode
+        //then comapares the elements using equals() and finally it will be able to find the book and able to remove from hash
+    }
+}
---
+++ src/test/java/com/pankz/bookshelf/BookShelfTest.java
@@ -0,0 +1,55 @@
+package com.pankz.bookshelf;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookShelfTest {
+    @Test
+    public void should_be_able_to_add_a_book_to_bookstore() {
+        BookShelf bookShelf = new BookShelf();
+        bookShelf.add(new Book("Effective Java"));
+        int count = bookShelf.numberOfBooks(); //Bookshelf should return the number of books
+        Assertions.assertThat(count).isEqualTo(1);
+
+    }
+    @Test
+    public void should_not_allow_same_book_to_be_added_to_bookshelf()
+    {
+        BookShelf bookShelf = new BookShelf();
+        bookShelf.add(new Book("Effective Java"));
+       //What should happen when same books added twice ?? it might throw an exception ..
+        try {
+            bookShelf.add(new Book("Effective Java"));
+            fail("This should not happen"); //code will never reach here can check by sysout below
+        } catch (Exception e) {
+            //Book with same title means your  arguments are same hence it will throw this exception
+            Assertions.assertThat(e).isInstanceOf(IllegalArgumentException.class);
+            Assertions.assertThat(e.getMessage()).isEqualTo("You can't add same book twice!!!");
+            //We are also trying to verify the message of that as well/verifying this msg from our code
+
+        }
+        System.out.println(bookShelf.numberOfBooks()); //o/p will be 2,therfore something is not right here
+       // IMPORTANT POINT-In Hashcode and set kind of stuff untill you didn't use equalsto() and hashcode()
+        //different(here 2) object will be created (no. of times you call each time diff. hashcode)
+        //different two object we are trying to save to hash set,it will by default save it
+        //We must implement hashcode() & equals() method
+        // Then only it will set two different hashcode for same values,otherwise it will do reference equality
+        //After implementing hashcode and equals() above o/p will be 1 . Hence it didn't added the 2nd book9coz same hashcode)
+        }
+        @Test
+    public void should_delete_an_existing_book_from_bookshelf()
+        {
+            BookShelf bookShelf = new BookShelf();
+            bookShelf.add(new Book("Effective Java"));
+            bookShelf.add(new Book("Clean Code"));
+           boolean deleted= bookShelf.delete(new Book("Effective Java"));
+           //Same here we set delete of type bollean ,it will return true if deleted otherwise false
+            //Same implementation as that of bollean added but here design is different,we can do as previous
+            //Here also as per the contract of remove method in "set.java"
+            Assertions.assertThat(deleted).isTrue();
+        }
+}
+
+
